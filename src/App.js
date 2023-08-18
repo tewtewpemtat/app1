a2bf062424847ca0102f36cfcaf77efebf3158e1
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2023-08-18 14:04:46 +0700

    add banner
    add banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { Header,Content,Footer } from './func-components';
 import Calendar from './class-components';
-function App() {
-  return (
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
-  );
-}
+import Banner from './banner';
 
-export default App;
+export default function App(){
+return  <Banner/>
+}
---
+++ src/banner.js
@@ -0,0 +1,37 @@
+import React from "react";
+function randInt(min,max){
+    let x = (max-min)+1
+    return min + Math.floor(Math.random()*x)
+}
+export default function Banner(){
+    const r = randInt(1,3)
+    let banner = ''
+    let url = ''
+
+    switch (r) {
+        case 1:
+            banner = './banner/react.jpg'
+            url = 'https://www.reactjs.org'
+            break
+        case 2:
+            banner = './banner/react-native.jpg'
+            url = 'https://www.reactnative.com'
+            break
+        case 3:
+            banner = './banner/node.jpg'
+            url = 'https://www.nodejs.org'
+            break
+        default :break
+}
+
+const bannerClick = () => {
+    window.open(url)
+    //window.location.href = url
+}
+return (
+    <div style={{margin:'20px',textAlign:'center'}}>
+        <img src={banner} onClick={bannerClick}style={{cursor:'pointer'}}
+        alt = "banner"/>
+    </div>
+)
+}
