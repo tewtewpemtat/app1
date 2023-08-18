55930a49283de185447aeba5c453b9b3e5c2dc22
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2023-08-18 14:57:46 +0700

    calculator add
    calculator add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Header,Content,Footer } from './func-components';
 import Calendar from './class-components';
+import {Calculator2} from './calculator'
 import Banner from './banner';
 
 export default function App(){
-return  <Banner/>
+  return <Calculator2/>
 }
